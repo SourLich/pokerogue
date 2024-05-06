953c45e447db8e1a4a01b8a878d9e550984d32c0
SourLich <74741165+SourLich@users.noreply.github.com>
2024-05-05 23:17:23 -0700

    Update overrides.ts

--- src/overrides.ts
+++ src/overrides.ts
@@ -7,19 +7,19 @@ import { WeatherType } from "./data/weather";
 export const SEED_OVERRIDE = '';
 export const STARTER_SPECIES_OVERRIDE = 0;
 export const STARTER_FORM_OVERRIDE = 0;
-export const STARTING_LEVEL_OVERRIDE = 0;
+export const STARTING_LEVEL_OVERRIDE = 100;
 export const STARTING_WAVE_OVERRIDE = 0;
-export const STARTING_BIOME_OVERRIDE = Biome.TOWN;
+export const STARTING_BIOME_OVERRIDE = Biome.END;
 export const STARTING_MONEY_OVERRIDE = 0;
 export const WEATHER_OVERRIDE = WeatherType.NONE;
 
 export const ABILITY_OVERRIDE = Abilities.NONE;
 export const MOVE_OVERRIDE = Moves.NONE;
 export const MOVE_OVERRIDE_2 = Moves.NONE;
-export const OPP_SPECIES_OVERRIDE = 0;
+export const OPP_SPECIES_OVERRIDE = 1006;
 export const OPP_ABILITY_OVERRIDE = Abilities.NONE;
 export const OPP_MOVE_OVERRIDE = Moves.NONE;
 export const OPP_MOVE_OVERRIDE_2 = Moves.NONE;
 
-export const OPP_SHINY_OVERRIDE = false;
+export const OPP_SHINY_OVERRIDE = true;
 export const OPP_VARIANT_OVERRIDE = 0;
